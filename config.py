8140eaddf262c5096a2dc4ea4cb3d8f36b0591d6
Vincent <vincent.jeanselme@gmail.com>
2019-03-05 15:50:47 -0500

    Add path to config

--- config.py
+++ config.py
@@ -5,6 +5,15 @@ processor = int(cpu_count()*1/2)
 time = datetime.datetime.now().strftime("%d %B %Y %H:%M:%S")
 
 print("Ran on {}".format(time))
-local = os.getcwd()
-datadir = os.path.join(local, "data_files/")
-result = os.path.join(local, "result/")
+
+if 'int.autonlab.org' in socket.gethostname():
+    print("Server")
+    datadir = "/zfsauton/data/public/vjeanselme/PennData/"
+    kerneldir = "/zfsauton/data/public/vjeanselme/PennKernels/"
+    result = "/zfsauton/data/public/vjeanselme/PennResults/"
+else:
+    print("Local")
+    local = os.getcwd()
+    datadir = os.path.join(local, "data_files/")
+    kerneldir  = os.path.join(local, "kernel_files/")
+    result = os.path.join(local, "result/")
