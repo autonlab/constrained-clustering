230ff55b9373ae5585fef2711d4007b68027cf5d
Jeanselme <vincent.jeanselme@gmail.com>
2020-03-03 15:06:33 -0500

    Allow to take best prediction for last iteration

--- optimizer.py
+++ optimizer.py
@@ -139,7 +139,8 @@ class ModelGuidedOptimization(CombinationKernelOptimizer):
             predictions, confidence = self.model.predict_confidence(potential_candidates)
             
             # Compute the best candidate
-            predictions += self.exploration * confidence
+            if step <= self.iteration - 1:
+                predictions += self.exploration * confidence
             index_candidate = np.argmax(predictions)
             self.candidates[step] = potential_candidates[index_candidate]
             self.scores[step] = self.objective_function(potential_candidates[index_candidate])
